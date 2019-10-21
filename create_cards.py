58e08cfa73b83a2be01e73ae46db09b228497c5b
Felipe <fgsouza93@hotmail.com>
2019-10-21 15:57:35 -0300

    fixed create_cards.py; cards are now checked from a list of strings with the card's names rather than an list with the cards objects

--- create_cards.py
+++ create_cards.py
@@ -1,27 +1,9 @@
 import sys
-
 from trello import TrelloClient
-
 from api import trello
 
-# Recieves the name of the desired label and the board where it
-# is contained. Returns the label object.
-def get_label(name, board):
-    label_list = board.get_labels()
-    # print('label_list', label_list)
-    for label in label_list:
-        if name in label.name: return label
-    
 if __name__ == "__main__":
 
-    # Initializadtion to be added in a contructor
-    client = TrelloClient(
-        api_key='ca43dd546a8464cf0b7564e0f392dbd1',
-        api_secret='dcda54138ad468433de04f0d422a4407e5dbfb84dad0198347c01bdab40dcde0',
-    )
-
-
-    # Code logic
     trello = trello.TrelloApi()
 
     member = trello.get_member('Felipe')
@@ -29,19 +11,24 @@ if __name__ == "__main__":
     members.append(member)
 
     labels = []
-    backlog = trello.my_lists[0]
+    backlog = trello.get_list('Backlog')
 
-    labels.append(get_label('LOW', trello.wls_board))
-    labels.append(get_label('OPS-FAIL', trello.wls_board))
-    labels.append(get_label('Project NMEDIA', trello.wls_board))
+    # Add a label to the card, the label should exist in the board. The first label that contains
+    # the selected string will be added.
+    labels.append(trello.get_label('LOW'))
+    labels.append(trello.get_label('OPS-FAIL'))
+    labels.append(trello.get_label('Project NMEDIA'))
 
     all_cards = []
-    all_cards = trello.get_all_cards()
+    all_cards_names = []
+    get_cards = trello.get_all_cards()
+    for card in get_cards:
+        all_cards_names.append(card.name)
+        
 
     if len(sys.argv) <= 1:
         print('\nMissing Parameter: Enter the file name containing the sources name')
         exit(1)
-
     filename = sys.argv[1]
     print('filename', filename)
 
@@ -49,10 +36,12 @@ if __name__ == "__main__":
         source_list = f.readlines()
     # you may also want to remove whitespace characters like `\n` at the end of each line
     source_list = [x.strip() for x in source_list] 
+    print(source_list)
   
     print("\nCreating new cards..\n")
+
     for source in source_list:
-        if source in all_cards:
+        if source in all_cards_names:
             print('Card already in the board:', source)
         else:
             print('Creating card for source:', source)
