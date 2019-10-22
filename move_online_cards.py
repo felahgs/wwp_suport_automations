1058d48e348e15a95fc7712d5458fa01c77c82ae
f.souza <cinq@carneiro.dom_qp.com.br>
2019-10-22 14:07:40 -0300

    move_online_cards.py: the card is now renamed with the current date, cards are now added at the bottom of the list

--- move_online_cards.py
+++ move_online_cards.py
@@ -3,6 +3,8 @@
 
 import sys
 
+from datetime import date
+
 from trello import TrelloClient
 
 from automation import wwp
@@ -18,28 +20,26 @@ def get_label(name, board):
 if __name__ == "__main__":
 
     # Initializadtion to be added in a contructor
-    client = TrelloClient(
-        api_key='ca43dd546a8464cf0b7564e0f392dbd1',
-        api_secret='dcda54138ad468433de04f0d422a4407e5dbfb84dad0198347c01bdab40dcde0',
-    )
+    # client = TrelloClient(
+        # api_key='ca43dd546a8464cf0b7564e0f392dbd1',
+        # api_secret='dcda54138ad468433de04f0d422a4407e5dbfb84dad0198347c01bdab40dcde0',
+    # )
 
-    all_boards = client.list_boards()
-    wls_board = all_boards[1]
-    my_lists = wls_board.list_lists()
+    # all_boards = client.list_boards()
+    # wls_board = all_boards[1]
+    # my_lists = wls_board.list_lists()
+
+    trello = trello.TrelloApi()
 
     # print(my_lists)
-    waiting_web_fetcher_list = [bucket for bucket in my_lists if "Waiting" in bucket.name]
-    done_list = [bucket for bucket in my_lists if "Live" in bucket.name]
+    waiting_web_fetcher_list = [bucket for bucket in trello.my_lists if "Waiting" in bucket.name]
+    done_list = [bucket for bucket in trello.my_lists if "Live" in bucket.name]
     print('Checking cards in', waiting_web_fetcher_list)
     print('Checking cards in', done_list)
 
-    trello = trello.TrelloApi()
-    card = trello.get_card('MEDIA_KH_8409')
-    # print(card)
-    # print(done_list[0].id)
+    # card = trello.get_card('WATCH_US_15471')
 
-    # new_name = card.name.split()[0] + ' - Done in 09/10/2019'
-    date = ' 10/10/2019'
+    date = date.today().strftime("%d/%m/%Y")
 
     automation = wwp.Portal()
     automation.login()
@@ -49,11 +49,12 @@ if __name__ == "__main__":
         status = automation.get_source_status(card_name)
         # print(card_name, status)
         if 'On-line' in status:
-            new_name = card_name + ' - Done in' + date
+            new_name = card_name + ' - Done in ' + date
             print(card_name, status)
             print(new_name)
             card.set_name(new_name)
             card.change_list(done_list[0].id)
+            card.change_pos("bottom")
             text = '**Automation: Moving Online Cards**\n' + 'Card ' + card_name + ' moved to ' + done_list[0].name
             card.comment(text)
             print(text, '\n')
