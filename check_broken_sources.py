7799a4fcf852a23c8dce0191687ca5c779be32f5
Felipe <fgsouza93@hotmail.com>
2019-10-24 09:38:27 -0300

    minor fixes on check_broken_sources.py

--- check_broken_sources.py
+++ check_broken_sources.py
@@ -18,24 +18,6 @@ from automation import wwp
 
 from utils import date
 
-# class dateChecker():
-#     def daterange(self, date1, date2):
-#         for n in range(int ((date2 - date1).days)+1):
-#             yield date1 + datetime.timedelta(n)
-
-#     def get_days_beetween(self, date1, date2):
-#         days = []
-#         for dt in self.daterange(date1, date2):
-#             # print(dt.strftime("%d/%m/%y"))
-#             days.append(dt.strftime("%d/%m/%Y"))
-#         return days
-
-#     def convert_to_date(self, date):
-#         dd = int(datetime.datetime.strptime(date, '%d/%m/%Y').strftime('%d'))
-#         mm = int(datetime.datetime.strptime(date, '%d/%m/%Y').strftime('%m'))
-#         yyyy = int(datetime.datetime.strptime(date, '%d/%m/%Y').strftime('%Y'))
-#         return datetime.date(yyyy,mm,dd)
-
 
 if __name__ == "__main__":
     
@@ -49,7 +31,7 @@ if __name__ == "__main__":
     done_cards = trello.get_done_cards(days)
     print(done_cards)
 
-    automation = Portal()
+    automation = wwp.Portal()
     automation.login()
     for card in done_cards:
         name = automation.get_source_name(card)
